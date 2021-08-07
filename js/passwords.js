629a77199c8b32277444991249079b9907214079
BuildTools <unconfigured@null.spigotmc.org>
2021-08-07 23:10:25 +0200

    Migrated delete account from passwords page to settings page

--- js/passwords.js
+++ js/passwords.js
@@ -11,10 +11,6 @@ document.getElementById("signout-link-mobile").innerText = lang[localStorage.lan
 document.getElementById("search").placeholder = lang[localStorage.lang]["search"];
 document.getElementById("add-password-btn").innerText = lang[localStorage.lang]["add_password"];
 
-document.getElementById("delete-account-title").innerText = lang[localStorage.lang]["delete_account"];
-document.getElementById("delete-account-text").innerText = lang[localStorage.lang]["delete_account_info"];
-document.getElementById("delete-account-btn").innerText = lang[localStorage.lang]["delete_account"];
-
 document.getElementById("dialog-button-cancel").innerText = lang[localStorage.lang]["cancel"];
 
 function displayPasswords(){
@@ -96,11 +92,6 @@ document.getElementById("dialog-button-cancel").addEventListener("click", () => 
     hide('dialog');
 });
 
-document.getElementById("delete-account-btn").addEventListener("click", () => {
-    changeDialog(1);
-    show('dialog');
-});
-
 document.getElementById("signout-link-mobile").addEventListener("click", () => {
     logout();
 });
@@ -158,20 +149,6 @@ function updateGeneratedPassword(upper, number, special){
 
 function changeDialog(style, text){
     switch(style){
-        case 1:
-            //Delete account dialog
-            document.getElementById('dialog-icon').className = "mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10";
-            document.getElementById('dialog-icon').innerHTML = "<svg class='h-6 w-6 text-red-600' xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke='currentColor' aria-hidden='true'><path stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z' /></svg>";
-    
-            document.getElementById('dialog-title').innerText = lang[localStorage.lang]["delete_account"];
-            document.getElementById('dialog-text').innerText = lang[localStorage.lang]["delete_account_confirmation"];
-    
-            document.getElementById('dialog-button-cancel').style.display = 'initial';
-
-            document.getElementById('dialog-button').className = "dangerButton inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 text-base font-medium focus:outline-none sm:w-auto sm:text-sm";
-            document.getElementById('dialog-button').innerText = lang[localStorage.lang]["delete"];
-            document.getElementById('dialog-button').onclick = () => deleteAccount();
-        break;
         case 2:
             //Add password error
             document.getElementById('dialog-icon').className = "mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10";
@@ -544,31 +521,6 @@ function deletePassword(password_id){
 
     };
     xhr.send("password_id=" + password_id);
-}
-
-function deleteAccount(){
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", localStorage.url + "/?action=deleteAccount");
-
-    xhr.setRequestHeader("Accept", "application/json");
-    xhr.setRequestHeader("Authorization", "Basic " + btoa(localStorage.username + ":" + sha512(localStorage.password)));
-    xhr.setRequestHeader("Content-Type", "application/json");
-
-    xhr.onreadystatechange = function () {
-
-        if(xhr.readyState === 4){
-            if(xhr.status != 200) return;
-            
-            const json = JSON.parse(xhr.responseText);
-
-            if(typeof json['error'] === 'undefined') return;
-            if(json['error'] != 0) return;
-            
-            logout();
-        }
-
-    };
-    xhr.send("");
 }
 
 function filterPasswords(){
