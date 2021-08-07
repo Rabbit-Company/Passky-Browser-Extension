e75385f4a010f8cce850327c46bfbe43c6c4ca27
BuildTools <unconfigured@null.spigotmc.org>
2021-08-07 18:51:01 +0200

    Working on Settings page

--- js/settings.js
+++ js/settings.js
@@ -1,7 +1,27 @@
-var qrcode = new QRCode("qrcode", {
-    text: "https://passky.org",
-    width: 512,
-    height: 512,
-    colorDark : "#ffffff",
-    colorLight : "#000000"
+if(!isSessionValid()) window.location.href = 'index.html';
+
+document.getElementById("passwords-link").innerText = lang[localStorage.lang]["passwords"];
+document.getElementById("import-export-link").innerText = lang[localStorage.lang]["import_export"];
+document.getElementById("signout-link").innerText = lang[localStorage.lang]["signout"];
+
+document.getElementById("passwords-link-mobile").innerText = lang[localStorage.lang]["passwords"];
+document.getElementById("import-export-link-mobile").innerText = lang[localStorage.lang]["import_export"];
+document.getElementById("signout-link-mobile").innerText = lang[localStorage.lang]["signout"];
+
+document.getElementById("dialog-button-cancel").innerText = lang[localStorage.lang]["cancel"];
+
+document.getElementById("signout-link").addEventListener("click", () => {
+    logout();
+});
+
+document.getElementById("signout-link-mobile").addEventListener("click", () => {
+    logout();
+});
+
+document.getElementById("main-menu-toggle-btn").addEventListener("click", () => {
+    toggleMenu();
+});
+
+document.getElementById("dialog-button-cancel").addEventListener("click", () => {
+    hide('dialog');
 });
