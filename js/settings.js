5ee7de6f6ae644228f146d6de5d45b3a14ade650
BuildTools <unconfigured@null.spigotmc.org>
2021-08-14 00:37:57 +0200

    Working on 2fa

--- js/settings.js
+++ js/settings.js
@@ -24,11 +24,9 @@ document.getElementById("settings-theme").value = localStorage.theme;
 document.getElementById("settings-session").value = localStorage.sessionDuration;
 
 if(localStorage.secret.length > 10){
-    console.log("bro lol wtf");
     document.getElementById("toggle-2fa-btn").innerText = "Disable";
     document.getElementById("toggle-2fa-btn").className = "dangerButton font-bold inline-flex items-center justify-center px-4 py-2 border border-transparent font-medium rounded-md focus:outline-none sm:text-sm";
 }else{
-    console.log("change");
     document.getElementById("toggle-2fa-btn").innerText = "Enable";
     document.getElementById("toggle-2fa-btn").className = "successButton font-bold inline-flex items-center justify-center px-4 py-2 border border-transparent font-medium rounded-md focus:outline-none sm:text-sm";
 }
