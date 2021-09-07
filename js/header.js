72602111763d70caaf9a2ef9b9b907e3567a6b3a
BuildTools <unconfigured@null.spigotmc.org>
2021-09-07 08:59:58 +0200

    Added Dracula theme

--- js/header.js
+++ js/header.js
@@ -2,7 +2,7 @@ if(localStorage.theme == null || typeof(localStorage.theme) == 'undefined') loca
 if(localStorage.lang == null || typeof(localStorage.lang) == 'undefined') localStorage.lang = "en";
 if(localStorage.sessionDuration == null || typeof(localStorage.sessionDuration) == 'undefined') localStorage.sessionDuration = 20;
 
-if(!(localStorage.theme == "dark" || localStorage.theme == "light" || localStorage.theme == "blue")) localStorage.theme = "dark";
+if(!(localStorage.theme == "dark" || localStorage.theme == "light" || localStorage.theme == "blue" || localStorage.theme == "dracula")) localStorage.theme = "dark";
 
 switch(localStorage.theme){
     case "light":
@@ -10,5 +10,8 @@ switch(localStorage.theme){
     break;
     case "blue":
         document.getElementById("css-theme").href = "css/themes/blue.css";
+    break;
+    case "dracula":
+        document.getElementById("css-theme").href = "css/themes/dracula.css";
     break;
 }
